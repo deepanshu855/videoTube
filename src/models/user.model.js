71b78f180ecdae4d6a6103c3a5830c6bcf85df21
Deepanshu Sharma <deepanshusharma040@gmail.com>
2025-08-01 20:08:21 +0530

    Hooks and methods in mongoose with JWT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema(
   {
@@ -47,5 +49,48 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encrypt the password.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Check the user entered password is same as the encrypted password.
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Now generate Access token.
+userSchema.methods.generateAccessToken = function () {
+  // Short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Now generate Refresh token.
+userSchema.methods.generateRefreshToken = function () {
+  // Saved in database
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
